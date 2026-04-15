af42054c50be0d80860b2126a8197480fad2feec
Devraj Khatiwada <devrajkhatiwada003@gmail.com>
2026-04-15 23:58:40 +0545

    feat: add MVP constitution flattening & update BM25 search

--- backend/text_processing.py
+++ backend/text_processing.py
@@ -1,7 +1,12 @@
 import re
+import sys
+from pathlib import Path
 
+ROOT_DIR = Path(__file__).resolve().parents[0]
+if str(ROOT_DIR) not in sys.path:
+    sys.path.insert(0, str(ROOT_DIR))
 
-from .src.constants import STOPWORDS
+from src.constants.stopwords import STOPWORDS
 
 
 def tokenize(text):
